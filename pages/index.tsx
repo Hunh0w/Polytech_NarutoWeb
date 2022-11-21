b3620857a282117e883f71c3a72ed1cae7b589b4
Vincent <vincent.font@etu.umontpellier.fr>
2022-11-21 16:37:40 +0100

    UI Building

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,6 @@
 import Head from 'next/head'
-// @ts-ignore
-import { Player } from 'video-react';
-import "node_modules/video-react/dist/video-react.css";
-import {useState} from "react";
 
 export default function Home() {
-
-  const [ shorts, setShorts ] = useState(["tobi_VS_konan.mp4", "itachi.mp4"]);
-
-  const shorts_elements = [];
-  for(let i = 0; i < shorts.length; i++){
-    shorts_elements.push(
-        <Player
-            key={i}
-            poster="/naruto.png"
-            fluid={false}
-            width={500}
-            height={500}
-            src={"/shorts/"+shorts[i]}
-        />
-    );
-  }
 
   return (
     <div>
@@ -30,9 +10,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div id="shorts">
-        {shorts_elements}
-      </div>
     </div>
   )
 }
