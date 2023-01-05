5bbc4ba8fdc1008145e5c7ff064333af22f623bd
Hunh0w <metasploitv2@gmail.com>
2023-01-05 10:09:51 +0100

    update: change host api address

--- src/utils/global_vars.ts
+++ src/utils/global_vars.ts
@@ -1,4 +1,4 @@
-export const url = "http://sigsegv:8080";
+export const url = "http://162.38.112.154:8080";
 
 export const isObjectEmpty = (object: any) => {
     for(let i in object) return false;
